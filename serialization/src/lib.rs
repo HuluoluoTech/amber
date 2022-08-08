3c7933ddbe4a9551a1c6bf437369169d21999586
zTgx <747674262@qq.com>
2022-08-08 11:26:46 +0800

    rename to amber

--- serialization/src/lib.rs
+++ serialization/src/lib.rs
@@ -1,13 +1,11 @@
 // import the generated code
 #[allow(dead_code, unused_imports)]
-#[path = "./game_generated.rs"]
+#[path = "../schema/game_generated.rs"]
 mod game_generated;
 
 pub use game_generated::*;
 
 pub fn serli() {
-    let mut builder = flatbuffers::FlatBufferBuilder::new_with_capacity(1024);
-
-
+    let mut _builder = flatbuffers::FlatBufferBuilder::with_capacity(1024);
 }
 
